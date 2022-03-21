96372193648542f052066fc6f4992113ac43b58e
chenmichael2 <chenmichael2012@gmail.com>
2022-03-20 20:57:03 -0700

    adding router, route, and routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Login from './components/Login';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    
+    <Router>
+      <App />
+
+      <Login />
+    </Router>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
