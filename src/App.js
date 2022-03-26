a8249c7589a210a949aef17fec9d902de3315eb1
chenmichael2 <chenmichael2012@gmail.com>
2022-03-25 21:50:47 -0700

    adding links to the home page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ function App() {
   return (
     <div className="App">
       <div>This is the introduction to our website</div>
+      <a href="/login">Login</a>
+      <a href="/budget">Budget</a>
     </div>
   );
 }
