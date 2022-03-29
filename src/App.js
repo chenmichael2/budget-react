7a97b194f32c185e980893eb568a03b024918138
chenmichael2 <chenmichael2012@gmail.com>
2022-03-28 20:21:04 -0700

    adding a sentence to the first page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
   return (
     <div className="App">
       <div>This is the introduction to our website</div>
-      <p></p>
+      <p>This will be in works until the project is around done</p>
       <a href="/login">Login</a>
       <br />
       <a href="/budget">Budget</a>
