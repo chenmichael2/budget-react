96bc4b18b2ab5bc4c78440338a6a665d25b8e0bc
chenmichael2 <chenmichael2012@gmail.com>
2022-03-28 20:20:32 -0700

    adding paragraph tags

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
   return (
     <div className="App">
       <div>This is the introduction to our website</div>
+      <p></p>
       <a href="/login">Login</a>
       <br />
       <a href="/budget">Budget</a>
